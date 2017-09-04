8db6125d39f6a8ba03f7be75af14ce8d4c216bdc
Leonardo Vinicius Kaminski Ferreira <leonardo@mindvalley.com>
2017-09-04 21:36:46 +0800

    Added audio extension and final configs to the project

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -7,6 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
+
+enum ButtonType: Int {
+    case slow = 0, fast, chipmunk, vader, echo, reverb
+}
 
 class PlaySoundsViewController: UIViewController {
     
@@ -18,12 +23,22 @@ class PlaySoundsViewController: UIViewController {
     @IBOutlet weak var reverbButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     
-    var recordedAudioURL: URL!
-
+    var recordedAudioURL:URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupAudio()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        configureUI(.notPlaying)
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,11 +49,28 @@ class PlaySoundsViewController: UIViewController {
     // MARK: Actions
     
     @IBAction func playSoundForButtonPressed(_ sender: UIButton) {
-        print("Play Sound Button Pressed")
+        
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        
+        configureUI(.playing)
+        
     }
     
     @IBAction func stopButtonPressed(_ sender: AnyObject) {
-        print("Stop Audio Button Pressed")
+        stopAudio()
     }
 
 }
