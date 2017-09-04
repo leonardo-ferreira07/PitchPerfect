8ef3084a1e85bfb75a77ec2afc823e353e5d72e6
Leonardo Vinicius Kaminski Ferreira <leonardo@mindvalley.com>
2017-09-04 19:10:56 +0800

    Added play sounds view controller

---
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Leonardo Vinicius Kaminski Ferreira on 04/09/17.
+//  Copyright © 2017 Leonardo Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
