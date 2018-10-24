c44c2690f27272f8dae7931dd8baeccc1dc1b31e
Leonardo Kaminski Ferreira <leonardo@mindvalley.com>
2018-10-23 21:41:09 -0300

    Conversion to Swift 4.2

--- PitchPerfect/View/Extensions/PlaySoundsViewController+Audio.swift
+++ PitchPerfect/View/Extensions/PlaySoundsViewController+Audio.swift
@@ -101,7 +101,7 @@ extension PlaySoundsViewController: AVAudioPlayerDelegate {
             
             // schedule a stop timer for when audio finishes playing
             self.stopTimer = Timer(timeInterval: delayInSeconds, target: self, selector: #selector(PlaySoundsViewController.stopAudio), userInfo: nil, repeats: false)
-            RunLoop.main.add(self.stopTimer!, forMode: RunLoopMode.defaultRunLoopMode)
+            RunLoop.main.add(self.stopTimer!, forMode: RunLoop.Mode.default)
         }
         
         do {
@@ -115,7 +115,7 @@ extension PlaySoundsViewController: AVAudioPlayerDelegate {
         audioPlayerNode.play()
     }
     
-    func stopAudio() {
+    @objc func stopAudio() {
         
         if let audioPlayerNode = audioPlayerNode {
             audioPlayerNode.stop()
